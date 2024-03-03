83fde3e7a8db92b2a63d6f64eb9c69a0264bda57
Sergio Ajiki <sergioajk@hotmail.com>
2024-03-03 16:12:24 -0300

    initial commit

---
+++ src/main/java/com/cadastrorh/cadastroRHapi/CadastroRhApiApplication.java
@@ -0,0 +1,13 @@
+package com.cadastrorh.cadastroRHapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroRhApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroRhApiApplication.class, args);
+	}
+
+}
