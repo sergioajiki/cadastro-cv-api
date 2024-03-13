184ba9662ee2313db5ee06142edf9b70b8aa04ce
Sergio Ajiki <sergioajk@hotmail.com>
2024-03-12 22:56:56 -0300

    feat<Dto>: infoCandidatoDto

---
+++ src/main/java/com/cadastrorh/cadastroRHapi/dto/InfoCandidatoDto.java
@@ -0,0 +1,87 @@
+package com.cadastrorh.cadastroRHapi.dto;
+
+import com.cadastrorh.cadastroRHapi.entity.Candidato;
+import jakarta.validation.constraints.NotBlank;
+
+import java.time.LocalDate;
+
+public record InfoCandidatoDto(
+        Long id,
+        String nome,
+        String sobrenome,
+        String cpf,
+//        String password,
+        LocalDate dataNascimento,
+        String genero,
+        String estadoCivil,
+        Boolean pcd,
+        String naturalidade,
+        String telCandidato,
+        String email,
+        String cep,
+        String estado,
+        String cidade,
+        String bairro,
+        String endereco,
+        Integer numero,
+        String complemento,
+        String contatoA,
+        String telContatoA,
+        String contatoB,
+        String telContatoB,
+        String foto,
+        Integer pretensaoSalarial,
+        Boolean possuiEmprego,
+        String cargoPretendido,
+        String tempoExperiencia,
+        Boolean possuiCnh,
+        String categoriaCnh,
+        Boolean possuiVeiculo,
+        Double alturaCandidato,
+        Double pesoCandidato,
+        Boolean possuiFilhos,
+        String idadeFilhos,
+        String curriculum,
+        String observacao
+) {
+    public static InfoCandidatoDto infoCandidatoToInfoCandidatoDto(Candidato candidato) {
+        return new InfoCandidatoDto(
+                candidato.getId(),
+                candidato.getNome(),
+                candidato.getSobrenome(),
+                candidato.getCpf(),
+                candidato.getDataNascimento(),
+                candidato.getGenero(),
+                candidato.getEstadoCivil(),
+                candidato.getPcd(),
+                candidato.getNaturalidade(),
+                candidato.getTelCandidato(),
+                candidato.getEmail(),
+                candidato.getCep(),
+                candidato.getEstado(),
+                candidato.getCidade(),
+                candidato.getBairro(),
+                candidato.getEndereco(),
+                candidato.getNumero(),
+                candidato.getComplemento(),
+                candidato.getContatoA(),
+                candidato.getTelContatoA(),
+                candidato.getContatoB(),
+                candidato.getTelContatoB(),
+                candidato.getFoto(),
+                candidato.getPretensaoSalarial(),
+                candidato.getPossuiEmprego(),
+                candidato.getCargoPretendido(),
+                candidato.getTempoExperiencia(),
+                candidato.getPossuiCnh(),
+                candidato.getCategoriaCnh(),
+                candidato.getPossuiVeiculo(),
+                candidato.getAlturaCandidato(),
+                candidato.getPesoCandidato(),
+                candidato.getPossuiFilhos(),
+                candidato.getIdadeFilhos(),
+                candidato.getCurriculum(),
+                candidato.getObservacao()
+        );
+    }
+}
