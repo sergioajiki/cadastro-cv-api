c98be067960108b9afdeaefa9838e7c2b04e3420
Sergio Ajiki <sergioajk@hotmail.com>
2024-03-12 21:13:50 -0300

    feat: ExperienciaDto

---
+++ src/main/java/com/cadastrorh/cadastroRHapi/dto/ExperienciaDto.java
@@ -0,0 +1,29 @@
+package com.cadastrorh.cadastroRHapi.dto;
+
+import com.cadastrorh.cadastroRHapi.entity.Experiencia;
+
+public record ExperienciaDto(
+        String cpfCandidato,
+        String empresa,
+        String cargoAnterior,
+        String contatoEmpresa
+) {
+    public static ExperienciaDto experienciaToExperienciaDto(Experiencia experiencia) {
+        return new ExperienciaDto(
+                experiencia.getCpfCandidato(),
+                experiencia.getEmpresa(),
+                experiencia.getCargoAnterior(),
+                experiencia.getContatoEmpresa()
+        );
+    }
+
+    public static Experiencia experienciaDtoToExperiencia(ExperienciaDto experienciaDto) {
+        Experiencia experiencia = new Experiencia();
+        experiencia.setCpfCandidato(experienciaDto.cpfCandidato);
+        experiencia.setEmpresa(experienciaDto.empresa);
+        experiencia.setCargoAnterior(experienciaDto.cargoAnterior);
+        experiencia.setContatoEmpresa(experienciaDto.contatoEmpresa);
+
+        return experiencia;
+    }
+}
