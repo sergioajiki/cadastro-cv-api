9de94811459ca663c2f506dce10a6d29601fe3c2
Sergio Ajiki <sergioajk@hotmail.com>
2024-03-19 00:16:51 -0300

    fix: ExperienciaDto

--- src/main/java/com/cadastrorh/cadastroRHapi/dto/ExperienciaDto.java
+++ src/main/java/com/cadastrorh/cadastroRHapi/dto/ExperienciaDto.java
@@ -1,16 +1,17 @@
 package com.cadastrorh.cadastroRHapi.dto;
 
+import com.cadastrorh.cadastroRHapi.entity.Candidato;
 import com.cadastrorh.cadastroRHapi.entity.Experiencia;
 
 public record ExperienciaDto(
-//        String cpfCandidato,
+        String cpfCandidato,
         String empresa,
         String cargoAnterior,
         String contatoEmpresa
 ) {
     public static ExperienciaDto experienciaToExperienciaDto(Experiencia experiencia) {
         return new ExperienciaDto(
-//                experiencia.getCpfCandidato(),
+                experiencia.getCandidato().getCpf(),
                 experiencia.getEmpresa(),
                 experiencia.getCargoAnterior(),
                 experiencia.getContatoEmpresa()
@@ -19,7 +20,8 @@ public record ExperienciaDto(
 
     public static Experiencia experienciaDtoToExperiencia(ExperienciaDto experienciaDto) {
         Experiencia experiencia = new Experiencia();
-//        experiencia.setCpfCandidato(experienciaDto.cpfCandidato);
+        Candidato candidato = new Candidato();
+        candidato.setCpf(experienciaDto.cpfCandidato);
         experiencia.setEmpresa(experienciaDto.empresa);
         experiencia.setCargoAnterior(experienciaDto.cargoAnterior);
         experiencia.setContatoEmpresa(experienciaDto.contatoEmpresa);
