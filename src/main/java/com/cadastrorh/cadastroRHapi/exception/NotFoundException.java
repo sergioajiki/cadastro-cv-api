df0ba6c915cd53dd926441d87fa335503361e67d
Sergio Ajiki <sergioajk@hotmail.com>
2024-05-01 17:12:22 -0300

    fea: NotFoundException

---
+++ src/main/java/com/cadastrorh/cadastroRHapi/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.cadastrorh.cadastroRHapi.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
