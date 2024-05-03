527b91d732f7f495269335351857d8f46f35c7ef
Sergio Ajiki <sergioajk@hotmail.com>
2024-05-03 13:57:33 -0300

    feat: AdminRepository

---
+++ src/main/java/com/cadastrorh/cadastroRHapi/repository/AdminRepository.java
@@ -0,0 +1,7 @@
+package com.cadastrorh.cadastroRHapi.repository;
+
+import com.cadastrorh.cadastroRHapi.entity.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+}
