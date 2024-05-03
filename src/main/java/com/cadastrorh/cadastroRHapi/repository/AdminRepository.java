a8db1c19005b9b3361ff9b015b27a03e415cf8f2
Sergio Ajiki <sergioajk@hotmail.com>
2024-05-03 15:07:32 -0300

    feat<AdminService>: registerAdmin salva as informações do adminstrador no BD

--- src/main/java/com/cadastrorh/cadastroRHapi/repository/AdminRepository.java
+++ src/main/java/com/cadastrorh/cadastroRHapi/repository/AdminRepository.java
@@ -4,4 +4,5 @@ import com.cadastrorh.cadastroRHapi.entity.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AdminRepository extends JpaRepository<Admin, Long> {
+    Admin findByEmail(String email);
 }
--- src/main/java/com/cadastrorh/cadastroRHapi/service/AdminService.java
+++ src/main/java/com/cadastrorh/cadastroRHapi/service/AdminService.java
@@ -1,8 +1,13 @@
 package com.cadastrorh.cadastroRHapi.service;
 
+import com.cadastrorh.cadastroRHapi.dto.AdminDto;
+import com.cadastrorh.cadastroRHapi.entity.Admin;
+import com.cadastrorh.cadastroRHapi.exception.DuplicateEntryException;
 import com.cadastrorh.cadastroRHapi.repository.AdminRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class AdminService {
@@ -12,4 +17,18 @@ public class AdminService {
     public AdminService(AdminRepository adminRepository) {
         this.adminRepository = adminRepository;
     }
+
+    public AdminDto registerAdmin(AdminDto adminDto) {
+        Admin adminToSave = AdminDto.AdminDtoToAdmin(adminDto);
+        Optional<Admin> adminOptional = Optional.ofNullable(adminRepository.findByEmail(adminDto.email()));
+        if (adminOptional.isPresent()) {
+            throw new DuplicateEntryException("Email is already registered");
+        }
+        adminRepository.save(adminToSave);
+        AdminDto savedAdmin = AdminDto.adminToAdminDto(adminToSave);
+
+        return savedAdmin;
+    }
+
+
 }
