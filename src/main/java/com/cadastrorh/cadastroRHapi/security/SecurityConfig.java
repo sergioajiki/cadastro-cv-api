80cbbe141e4d2e8246fac56f2da4b476df0b6b8c
Sergio Ajiki <sergioajk@hotmail.com>
2024-05-04 19:30:25 -0300

    feat<SecurityConfig>: PasswordEncoder

--- src/main/java/com/cadastrorh/cadastroRHapi/security/SecurityConfig.java
+++ src/main/java/com/cadastrorh/cadastroRHapi/security/SecurityConfig.java
@@ -13,6 +13,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.HandlerExceptionResolver;
@@ -67,6 +69,10 @@ public class SecurityConfig {
                 .build();
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
     @Bean
     public AuthenticationManager authenticationManager(
             AuthenticationConfiguration authenticationConfiguration
